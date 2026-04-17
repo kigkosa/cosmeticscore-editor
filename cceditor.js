baaa2a5b93ecfaed693ce2802fca969607f67738
phisut kosayamat <dady_ocat@hotmail.com>
2026-04-17 10:11:07 +0700

    update version

--- cceditor.js
+++ cceditor.js
@@ -8,7 +8,7 @@
 		author: "LoneDev",
 		description: "Utility to easily edit and preview CosmeticsCore cosmetics.",
 		icon: icon,
-		version: "1.2.3",
+		version: "1.2.4",
 		variant: "both",
         min_version: "5.0.0",
 		onload: onload,
